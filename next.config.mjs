58810964f2118645481805c4a118b7e9f4572ef1
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-03-24 02:51:53 +0200

    offline use update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  dest: 'public', // Output directory for the service worker
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+});
 
 export default nextConfig;
